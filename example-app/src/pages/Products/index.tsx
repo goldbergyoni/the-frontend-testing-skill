dbde49d467ec4b0d63a0b12d9ecd54096fc23865
Yoni Goldberg <me@goldbergyoni.com>
2025-12-30 10:48:15 +0200

    Genric message

--- example-app/src/pages/Products/index.tsx
+++ example-app/src/pages/Products/index.tsx
@@ -1,9 +1,7 @@
 import { Button } from "@chakra-ui/react";
 import { useState } from "react";
 
-import { useProductsFilter } from "@/features/products/application/use-products-filter";
 import { useProductsQuery } from "@/features/products/infrastructure/productsQuery";
-import { ProductsFilter } from "@/features/products/presentation/ProductsFilter";
 import { ProductsList } from "@/features/products/presentation/ProductsList";
 import { Page } from "@/lib/components/Layout/Page";
 import { PageHeader } from "@/lib/components/Layout/PageHeader";
@@ -21,22 +19,12 @@ const ProductsPage = () => {
     keepPreviousData: true,
   });
 
-  const { filters, setFilters, filteredProducts, resetFilters, hasActiveFilters } =
-    useProductsFilter(data.products);
-
   const noMoreProducts = data.meta.total <= params.limit;
 
   return (
     <Page>
-      <PageHeader title={t("title")} description={t("description")}>
-        <ProductsFilter
-          filters={filters}
-          setFilters={setFilters}
-          resetFilters={resetFilters}
-          hasActiveFilters={hasActiveFilters}
-        />
-      </PageHeader>
-      <ProductsList products={filteredProducts} />
+      <PageHeader title={t("title")} description={t("description")} />
+      <ProductsList products={data.products} />
       {data.products.length > 0 && (
         <Button
           w="100%"
